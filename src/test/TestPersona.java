afc974ee8598e4eb42f0e396b2c5bd7afa002602
paoli7612 <paoli7612@gmail.com>
2021-06-29 04:30:24 +0200

    testing of Persona class

---
+++ src/test/TestPersona.java
@@ -0,0 +1,17 @@
+package test;
+
+import utils.Persona;
+
+public final class TestPersona extends Test{
+	
+	public TestPersona() {
+		super("persona");
+		
+		Persona p = new Persona("Tommaso", "Paoli");
+		
+		p.print();
+		
+		end();
+	}
+
+}
