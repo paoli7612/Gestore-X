4d2a6bc1b9453439f384e76e702a973be75dffd8
paoli7612 <paoli7612@gmail.com>
2021-10-10 22:40:27 +0200

    chiedi credenziali in user class

--- src/App.java
+++ src/App.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
+import app.Account;
+
 public final class App {
 
-	static String email;
-	static String password;
+	static Account account;
 	
-	public static void main(String[] args) {
-		
-		if (args.length == 2) {
-			email = args[0];
-			password = args[1];
-		} else {
-			
-			Scanner sc = new Scanner(System.in);
-			
-			System.out.print("email:    ");
-			email = sc.nextLine();
-			System.out.print("password: ");
-			password = sc.nextLine();
+	public static void main(String[] args) {		
+		try {
+			if (args.length == 2) {
+				account = new Account(args[0], args[1]);
+			} else {
+				Scanner sc = new Scanner(System.in);
+				
+				System.out.print("email:     ");
+				String email = sc.nextLine();
+				System.out.print("password:  ");
+				String password = sc.nextLine();
+				
+				account = new Account(email, password);
+			}
+		} catch (Exception e) {
+			System.out.println("Credenziali non valide");
+			return;
 		}
-		
-		System.out.println("Email: " + email + " -  Password: " + password);
-		
 		
 	}
 
