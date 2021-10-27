60facb4f081c6389d077ee12c3b7cdbf88465e4d
Tommaso Paoli <tomaoli7612@gmail.com>
2021-10-27 22:25:13 +0200

    Persona

---
+++ src/app/Persona.java
@@ -0,0 +1,18 @@
+package app;
+
+public class Persona {
+
+	private String nome;
+	private String cognome;
+	
+	public Persona(String nome, String cognome) {
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+	
+	@Override
+	public String toString() {
+		return nome + " " + cognome;
+	}
+	
+}
